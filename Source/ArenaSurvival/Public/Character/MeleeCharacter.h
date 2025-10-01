86be56691d8cf7c0786871b5846ed1d591e8c8ec
Barış Ayık <bybarqunie@gmail.com>
2025-10-01 18:58:39 +0300

    Add MeleeCharacter class and update auto attack asset
    Introduced AMeleeCharacter class inheriting from AArenaCharacterBase with header and source files. Updated GA_AutoAttack.uasset, likely to support melee character functionality.

---
+++ Source/ArenaSurvival/Public/Character/MeleeCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/ArenaCharacterBase.h"
+#include "MeleeCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENASURVIVAL_API AMeleeCharacter : public AArenaCharacterBase
+{
+	GENERATED_BODY()
+	
+};
